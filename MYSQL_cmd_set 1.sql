0d987b8e2ce8e5eda7e302d0b5d30ee07ca9855b
Mrunal Bokil <37785453+mrunalb09@users.noreply.github.com>
2020-12-29 15:16:47 -0800

    Create MYSQL_cmd_set 1.sql

---
+++ MYSQL_cmd_set 1.sql
@@ -0,0 +1,18 @@
+/* Revising the Select Query I */
+select * from CITY where CountryCode = 'USA' and Population > 100000
+
+/* Revising the Select Query II */
+select name from city where CountryCode = 'USA' and population > 120000 
+
+/* Select All */
+select * from city
+
+/* Select By ID */
+select * from city where id = 1661
+
+/* Japanese Cities' Attributes */
+select * from city where countrycode = 'JPN'
+
+/* Japanese Cities' Names */
+select name from city where countrycode = 'JPN' 
+
